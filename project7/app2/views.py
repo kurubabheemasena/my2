9f95563168446f73f174d17340e61061086130ca
kurubabheemasena <kbheemasena15165@gmail.com>
2022-12-26 14:57:01 +0530

    first commit

---
+++ project7/app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def bheemasen(request):
+    return HttpResponse('<marque><h1>bheemasena</maequee><h1>')
+    
